dca1f9a5234b48faf8472146a514e7f898f7997e
Gustav Eikaas <gustav.eikaas@gmail.com>
2025-04-03 20:10:47 +0200

    .

--- lua/code-playground/commands.lua
+++ lua/code-playground/commands.lua
@@ -95,7 +95,7 @@ local function open_workspace(file, command)
 	local stdout = createStdoutBuf(buf)
 
 	local function run()
-    vim.cmd("w!")
+		vim.cmd("w!")
 		local lines = {}
 
 		local anim_timer = animation[options_manager.options.animation](function(frame)
